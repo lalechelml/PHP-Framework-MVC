c384667182617ffd1321a33b299d50aab75fa534
lalechelml <ramos.joshelin@hotmail.com>
2020-09-14 22:22:35 -0500

    WEBSITE_name

--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -5,11 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="<?php echo URL_PATH ?>/css/estilos.css">
-  <title><?php WEBSITE_NAME; ?></title>
+  <title><?php echo WEBSITE_NAME; ?></title>
 </head>
 
 <body>
-
-</body>
-
-</html>
